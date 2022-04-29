cf122b45b21762e413c2b5782829ff5e13dca08a
Francisco Huapaya <franciscojho@hotmail.com>
2022-04-29 12:49:17 -0400

    isolate express app

--- src/index.ts
+++ src/index.ts
@@ -1,23 +1,11 @@
 /* eslint-disable no-console */
 
-import 'dotenv/config'
-import express from 'express'
-import cors from 'cors'
-import { authRouter } from './routes/auth.route'
-import { errorHandler } from './middlewares/errorHandler'
-import { dbConnection } from './config/database'
+import { app } from './app'
+import { connect } from './config/database'
 
-const app = express()
 const port = process.env.PORT || 8000
 
-app.use(cors())
-app.use(express.json())
-
-app.use('/auth/local', authRouter)
-
-app.use(errorHandler)
-
-dbConnection()
+connect()
     .then((resp) => console.log(resp))
     .catch((err) => console.log(err))
 
