516a01348d05bf8f18a77e55ad568847d74a84aa
Francisco Huapaya <franciscojho@hotmail.com>
2022-05-01 19:51:03 -0400

    add middleware to validate auth

---
+++ src/middlewares/isAuthenticated.ts
@@ -0,0 +1,14 @@
+import { Request, Response, NextFunction } from 'express'
+import { verify } from 'jsonwebtoken'
+import { JwtPayload } from '../interfaces'
+
+export const isAuthenticated = (req: Request, res: Response, next: NextFunction) => {
+    const secret = process.env.JWT_SECRET as string
+    const bearerToken = req.headers.authorization
+    if (bearerToken) {
+        const token = bearerToken.split(' ')[1]
+        const decoded = verify(token, secret) as JwtPayload
+        if (decoded.id === req.body.userId) return next()
+    }
+    res.status(401).json({ message: 'Unauthorized' })
+}
